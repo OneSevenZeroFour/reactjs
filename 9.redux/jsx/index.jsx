fbc59c88a6d113f1fac93de8df473b51a18d8ebf
wscats <2587575267@qq.com>
2017-10-19 11:27:41 +0800

    redux

---
+++ 9.redux/jsx/index.jsx
@@ -0,0 +1,68 @@
+var store = Redux.createStore((state, action) => {
+  // store
+  if (typeof state === 'undefined') {
+    return {name: "abc", skill: "ps"}
+  }
+  // action
+  switch (action.type) {
+    case "SETNAME":
+      return {name: "cba", skill: "css"}
+      break;
+    default:
+  }
+})
+var Xheader = React.createClass({
+  getInitialState() {
+    return {name: "laoyao", answer: "", selectValue: "2"}
+  },
+  setName() {
+    store.dispatch({type: "SETNAME"})
+  },
+  render() {
+    return (
+      <div ref="name" style={{
+        border: "1px solid green",
+        padding: "20px"
+      }}>
+        <div>第一个组件</div>
+        <button onClick={this.setName}>Ok</button>
+      </div>
+    )
+  },
+  //挂载之后 mounted
+  componentDidMount() {
+    // this.$store.getter
+    // this.$store.dispatch
+    console.log(store.getState());
+  }
+})
+var Xfooter = React.createClass({
+  getInitialState() {
+    return {name: "第二个组件"}
+  },
+  render() {
+    return (
+      <div ref="name" style={{
+        border: "1px solid green",
+        padding: "20px"
+      }}>
+        <div>{this.state.name}</div>
+      </div>
+    )
+  },
+  //挂载之后 mounted
+  componentDidMount() {
+    var self = this;
+    // computed
+    store.subscribe(() => {
+      console.log(store.getState());
+      self.setState({name: store.getState().name})
+    })
+  }
+})
+
+ReactDOM.render(
+  <div>
+  <Xheader/>
+  <Xfooter/>
+</div>, document.querySelector('.example'));
